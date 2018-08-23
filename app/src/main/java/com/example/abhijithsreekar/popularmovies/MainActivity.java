5a9672f3330ab5d0cdbe710c80825cf1a85e4ff1
Abhijith Sreekar <abhijith.sreekar@infra.tigerspike.com>
2018-08-24 01:24:07 +0800

    color change for action bar and status bar

--- app/src/main/java/com/example/abhijithsreekar/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/abhijithsreekar/popularmovies/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.abhijithsreekar.popularmovies;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
